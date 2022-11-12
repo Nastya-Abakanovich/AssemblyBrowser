f485e65b8e0f7ce9b12c9c8949f406c139ad5944
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-11-12 18:27:09 +0300

    Added MVVM ViewModel.

---
+++ AssemblyBrowserApplication/ViewModel/ViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using AssemblyBrowserApplication.Model;
+using AssemblyBrowserLibrary.AssemblyComposition;
+
+
+namespace AssemblyBrowserApplication.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private string _assemblyPath = "";
+        private List<Node> _assemblyTree;
+
+        public string AssemblyPath
+        {
+            get => _assemblyPath;
+            set
+            {
+                _assemblyPath = value;
+                // D:\Университет\5 сем\СПП\Лабы\ExampleLibrary\ExampleLibrary\bin\Debug\net6.0\ExampleLibrary.dll
+                AssemblyTree = AssemblyInfoToTreeConverter.Convert(Core.GetTree(AssemblyPath));                
+                OnPropertyChanged(nameof(AssemblyTree));
+            }
+        }
+
+        public List<Node> AssemblyTree
+        {
+            get => _assemblyTree;
+            set => _assemblyTree = value;
+        }
+
+        private OpenFileDialogCommand? _openFileDialog;
+        public OpenFileDialogCommand OpenFileDialog
+        {
+            get
+            {
+                return _openFileDialog ??
+                  (_openFileDialog = new OpenFileDialogCommand(o => OpenAssembly()));
+            }
+        }
+
+        private void OpenAssembly()
+        {
+
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Assembly";
+            dialog.DefaultExt = ".dll";
+            dialog.Filter = "Dynamic link library (.dll)|*.dll";
+            dialog.Title = "Select assembly";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                AssemblyPath = dialog.FileName;
+                OnPropertyChanged(nameof(AssemblyPath));
+            }
+        }
+    }
+}
+
+
