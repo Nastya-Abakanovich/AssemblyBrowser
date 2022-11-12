a7dd493fa21d8f7c343551ad3c827eb5719de4ec
Nastya-Abakanovich <nastya.abakanovich@gmail.com>
2022-11-12 18:17:26 +0300

    Added MVVM View.

--- AssemblyBrowserApplication/View/MainWindow.xaml.cs
+++ AssemblyBrowserApplication/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AssemblyBrowserApplication
+namespace AssemblyBrowserApplication.View
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
